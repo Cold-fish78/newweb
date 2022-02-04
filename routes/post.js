f6dc61052cc5fdf171b4f481399ccafefac7bb10
Cold-fish78 <pubg69086@gmail.com>
2022-02-04 16:25:37 +0530

    added a blog

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/post_controller');
-router.get('/blogpost',postController.blogpost);
+router.get('/aws-not-accepting-debit-credit-card',postController.blogpost);
 router.get('/search',postController.search);
 
 
