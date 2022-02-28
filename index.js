ff0f3b65e7c1ac7e45f656fb31b1427cb0428e6b
Cold-fish78 <pubg69086@gmail.com>
2022-02-28 15:03:06 +0530

    added robot.txt and sitemap get routes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 8004;
 app.use(express.urlencoded({extended : true}));
 
 require("dotenv").config();
@@ -9,6 +9,41 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('./assets'));
 //  for routes;
+const path = require('path');
+app.get('/robot.txt',function(req,res){
+    var options = {
+        root: path.join(__dirname , )
+    };
+     
+    var fileName = 'robot.txt';
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+        console.log(
+
+            "error occured" + err
+        )
+        } else {
+            console.log('sent')
+        }
+    });
+});
+app.get('/sitemap.xml',function(req,res){
+    var options = {
+        root: path.join(__dirname , )
+    };
+     
+    var fileName = 'sitemap.xml';
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+        console.log(
+
+            "error occured" + err
+        )
+        } else {
+            console.log('sent')
+        }
+    });
+})
 // app.use('/',('./routes'));
 app.use('/',require('./routes/index'));
 
