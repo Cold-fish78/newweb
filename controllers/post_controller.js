849615045b502bfb3b5f50da26e7e21b117b5426
Cold-fish78 <pubg69086@gmail.com>
2022-03-12 12:48:38 +0530

    added a new post

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -7,3 +7,6 @@ module.exports.search = function(req,res){
 module.exports.blogpost2 = function(req,res){
     return res.render('razorpayIntegration',{ title : 'razorpay integration with node js'});
 }
+module.exports.blogpost3 = function(req,res){
+    return res.render('weatherApp',{ title : 'create weather app with flutter'});
+}
--- routes/post.js
+++ routes/post.js
@@ -4,5 +4,6 @@ const postController = require('../controllers/post_controller');
 router.get('/aws-not-accepting-debit-credit-card',postController.blogpost);
 router.get('/search',postController.search);
 router.get('/razorpay-nodejs-integration',postController.blogpost2);
+router.get('/flutter-weather-app',postController.blogpost3);
 
 module.exports = router;
